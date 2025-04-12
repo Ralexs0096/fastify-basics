d557d2e1f4b7e4b453423f3dec6cb1803457de9c
Alexs Ruiz <ralexs.acu@gmail.com>
2025-04-11 23:46:27 -0600

    Valida the request and add a preHandler example

--- index.js
+++ index.js
@@ -9,6 +9,38 @@ fastify.get('/', function (_, reply) {
   reply.send({ hello: 'Krys' });
 });
 
+// JSON SCHEMA
+const schema = {
+  body: {
+    type: 'object',
+    required: ['someKey'],
+    properties: {
+      someKey: { type: 'boolean' },
+      someOtherKey: { type: 'number' }
+    }
+  }
+};
+
+fastify.post(
+  // PATH
+  '/',
+  // options > Schema validator
+  {
+    schema,
+    preHandler: function (req, reply, done) {
+      console.log('Hello from pre-handler!', req.body.someKey);
+      done();
+    }
+  },
+  // Handler
+  async function (req, reply) {
+    const { someKey } = req.body;
+    reply.send({
+      someKey
+    });
+  }
+);
+
 /**
  * Run the server!
  */
